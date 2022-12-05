5c308e809c60a3cf0e6712507032fa68376e187a
samirjabib <samirdev9669@gmail.com>
2022-12-04 19:41:09 -0500

    add controller for updateEvent

--- components/events/events.controller.js
+++ components/events/events.controller.js
@@ -11,7 +11,12 @@ const Event = require('./events.model');
 
 
 const getEvents = async (req, res = response, next) => {
-   
+    const events = await Event.find()
+                                .populate('user', 'name') // select the information you want see 
+    res.status(200).json({
+        status:"success",
+        events,
+    })
 };
 
 
@@ -24,25 +29,57 @@ const createEvent = async (req, res = response, next) => {
 
         const eventSaved = await event.save();
     
-        res.status(500).json({
+        res.status(201).json({
             status:'success',
             event: eventSaved,
         })
     } catch (error) {
         console.log(error)
-        res.status(500).json({
-            status:'failed',
-            msg:'contact admin for more info.'
-        })
+        handleHttpError(res,"ERROR_CREATE_EVENT",500)
     }
    
 }
 
 const updateEvent = async (req, res, next) => {
-    res.json({
-        status:'success',
-        msg:'even has been update'
-    })
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const event = await Event.findById( eventId );
+
+        if(!event){
+            return res.status(404).json({
+                status:"success",
+                msg:"The event dont exist by this id"
+            });
+        };
+
+        if( event.user.toString() !== uid) {
+            return res.status(401).json({
+                status:"failed",
+                msg:"you do not have permission on this account "
+            })
+        };
+
+        const newEvent = {
+            ...req.body,
+            user:uid,
+        };
+
+        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent,{ new:true });
+
+        res.status(200).json({
+            status:"success",
+            event: eventUpdated,
+        })
+
+    } catch (error) {
+        console.log(error)
+        handleHttpError(res, "ERROR UPDATE EVENT")
+    }
+
 }
 
 const deleteEvent = async (req, res, next) => {
